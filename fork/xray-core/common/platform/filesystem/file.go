22566562ea9c149ccdfa8f5004563be15d871e09
DemirSe <demir@localhost>
2026-09-09 20:47:24 +0300

    ponytail: inline filesystem.ReadFile into ReadCert via os.ReadFile

--- fork/xray-core/common/platform/filesystem/file.go
+++ fork/xray-core/common/platform/filesystem/file.go
@@ -4,23 +4,12 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/xtls/xray-core/common/buf"
 	"github.com/xtls/xray-core/common/platform"
 )
 
-func ReadFile(path string) ([]byte, error) {
-	reader, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer reader.Close()
-
-	return buf.ReadAllToBytes(reader)
-}
-
 func ReadCert(file string) ([]byte, error) {
 	if filepath.IsAbs(file) {
-		return ReadFile(file)
+		return os.ReadFile(file)
 	}
-	return ReadFile(platform.GetCertLocation(file))
+	return os.ReadFile(platform.GetCertLocation(file))
 }
